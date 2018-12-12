44ea4140d7ed524632c4510ee959ce35763a45de
Jo-Jo Feng <yanlin.feng@yale.edu>
2018-12-11 22:52:59 -0500

    Updates

---
+++ precache-manifest.c340ff749845403601d01a06d1fb5ee8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8755865b9293de0bd72c",
+    "url": "/bulldogenergy/static/js/runtime~main.8755865b.js"
+  },
+  {
+    "revision": "24e5b6a6297e2c70a06e",
+    "url": "/bulldogenergy/static/js/main.24e5b6a6.chunk.js"
+  },
+  {
+    "revision": "65ea75562610ade22fa3",
+    "url": "/bulldogenergy/static/js/1.65ea7556.chunk.js"
+  },
+  {
+    "revision": "24e5b6a6297e2c70a06e",
+    "url": "/bulldogenergy/static/css/main.9b2c4835.chunk.css"
+  },
+  {
+    "revision": "3f1177e00fae02f011bfb2b966b4db84",
+    "url": "/bulldogenergy/index.html"
+  }
+];
