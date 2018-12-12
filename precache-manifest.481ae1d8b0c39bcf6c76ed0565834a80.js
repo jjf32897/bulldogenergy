e50f87e22c8429483fba0255b24fe958e9e59b5a
Jo-Jo Feng <yanlin.feng@yale.edu>
2018-12-12 02:24:25 -0500

    Updates

--- precache-manifest.481ae1d8b0c39bcf6c76ed0565834a80.js
+++ precache-manifest.481ae1d8b0c39bcf6c76ed0565834a80.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/bulldogenergy/static/js/runtime~main.8755865b.js"
   },
   {
-    "revision": "f33fcdb71b832c620a0c",
-    "url": "/bulldogenergy/static/js/main.f33fcdb7.chunk.js"
+    "revision": "02f75a7fa2e587eebe30",
+    "url": "/bulldogenergy/static/js/main.02f75a7f.chunk.js"
   },
   {
     "revision": "65ea75562610ade22fa3",
     "url": "/bulldogenergy/static/js/1.65ea7556.chunk.js"
   },
   {
-    "revision": "f33fcdb71b832c620a0c",
+    "revision": "02f75a7fa2e587eebe30",
     "url": "/bulldogenergy/static/css/main.9b2c4835.chunk.css"
   },
   {
-    "revision": "1004b74823b4c921a421848e7b813cb0",
+    "revision": "97574e37b62121b947086d7e64197635",
     "url": "/bulldogenergy/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bulldogenergy/precache-manifest.71dd22c7c0e329f64eea0ecf97190584.js"
+  "/bulldogenergy/precache-manifest.481ae1d8b0c39bcf6c76ed0565834a80.js"
 );
 
 workbox.clientsClaim();
